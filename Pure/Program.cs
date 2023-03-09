5cb21b30fc0a54249289d9df70b61572956bcbf3
Charles Zhang <szinubuntu@gmail.com>
2023-03-09 14:51:33 -0500

    [Pure] Add interactive mode to scripts

--- Pure/Program.cs
+++ Pure/Program.cs
@@ -10,16 +10,18 @@ namespace Pure
                 new Interpreter().Start("""
                     Pure v0.0.1
                     """);
-            else if (args.Length == 1) 
+            else if (args.Length >= 1) 
             {
-                string file = Path.GetFullPath(args[0]);
+                bool interactiveMode = args.Length == 2 && (bool.Parse(args[0]) || args[0].ToLower() == "-i" || args[0].ToLower() == "--interactive");
+                string fileName = args.Length == 2 ? args[1] : args[0];
+
+                string file = Path.GetFullPath(fileName);
                 if (!File.Exists(file))
                 {
                     Console.WriteLine($"File {file} doesn't exist.");
                     return; 
                 }
-                // TODO: Currently we are NOT properly parsing the file - we assume only line commands
-                new Interpreter().Start(string.Empty, false, true, Parser.SplitScripts(File.ReadAllText(file)), true);
+                new Interpreter().Start(string.Empty, false, true, Parser.SplitScripts(File.ReadAllText(file)), !interactiveMode);
             }
         }
     }
