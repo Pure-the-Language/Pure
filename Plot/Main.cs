cb22b61b7da624f76952b06d4f364b1f16495133
Charles Zhang <szinubuntu@gmail.com>
2023-03-10 08:34:47 -0500

    [Library] Preliminary plotting

---
+++ Plot/Main.cs
@@ -0,0 +1,13 @@
+﻿namespace Plot
+{
+    public static class Main
+    {
+        public static void Scatter(double[] x, double[] y, params string[] settings)
+        {
+            var plt = new ScottPlot.Plot(400, 300);
+            plt.AddScatter(x, y);
+            new ScottPlot.WpfPlotViewer(plt).ShowDialog();
+            // plt.SaveFig("quickstart.png");
+        }
+    }
+}
---
+++ Plot/README.md
@@ -0,0 +1,8 @@
+﻿# Plot Library
+
+Provides single-entry straightforward 2D plotting functionalities for numerical data: `Plot(XAxis, AXisSeries, Settings)`.
+
+Variations:
+
+* `Plot(X, Y, Settings)`
+* `Plot(X, Y1-Y20, Settings)`
