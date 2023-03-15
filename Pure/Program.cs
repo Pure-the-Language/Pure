311df5a6c34d9ec3429abc2d551483fb8b43d474
Charles Zhang <szinubuntu@gmail.com>
2023-03-15 13:55:23 -0400

    [Pure] Add immediate mode (-m/-mi)
    - Add support for arguments

--- Pure/Program.cs
+++ Pure/Program.cs
@@ -10,10 +10,14 @@ namespace Pure
                 new Interpreter().Start("""
                     Pure v0.0.2
                     """);
+            else if (args.Length >= 2 && (args[0] == "-m" || args[0] == "-mi"))
+            {
+                new Interpreter().Start(string.Empty, false, true, new string[] { args[1] }, args[0] == "-mi", args.Skip(2).ToArray());
+            }
             else if (args.Length >= 1) 
             {
-                bool interactiveMode = args.Length == 2 && (bool.Parse(args[0]) || args[0].ToLower() == "-i" || args[0].ToLower() == "--interactive");
-                string fileName = args.Length == 2 ? args[1] : args[0];
+                bool interactiveMode = args.Length >= 2 && (bool.Parse(args[0]) || args[0].ToLower() == "-i" || args[0].ToLower() == "--interactive");
+                string fileName = interactiveMode ? args[1] : args[0];
 
                 string file = Path.GetFullPath(fileName);
                 if (!File.Exists(file))
@@ -21,7 +25,7 @@ namespace Pure
                     Console.WriteLine($"File {file} doesn't exist.");
                     return; 
                 }
-                new Interpreter().Start(string.Empty, false, true, Parser.SplitScripts(File.ReadAllText(file)), !interactiveMode);
+                new Interpreter().Start(string.Empty, false, true, Parser.SplitScripts(File.ReadAllText(file)), !interactiveMode, interactiveMode ? args.Skip(2).ToArray() : args.Skip(1).ToArray());
             }
         }
     }
