8e8d5f5bbe4494dec0c8ebd3612696e4a84e6aa7
Charles Zhang <szinubuntu@gmail.com>
2023-03-16 13:40:40 -0400

    [Python] Preliminary standard Python module

---
+++ Python/Main.cs
@@ -0,0 +1,44 @@
+﻿using Python.Runtime;
+
+namespace Python
+{
+    public static class Main
+    {
+        #region Initialization
+        private static PyModule Scope;
+        private static void StartUp()
+        {
+            try
+            {
+                Python.Runtime.Runtime.PythonDLL = "python310.dll";
+                PythonEngine.Initialize();
+                Scope = Py.CreateScope();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        private static void ShutDown()
+        {
+            Scope.Dispose();
+            PythonEngine.Shutdown();
+        }
+        #endregion
+
+        public static void Evaluate(string snippet)
+        {
+            if (Scope == null) 
+            {
+                Console.WriteLine("Python runtime is not initialized.");
+                return;
+            }
+            using (Py.GIL())
+            {
+                dynamic result = Scope.Eval(snippet);
+                if (result != null)
+                    Console.WriteLine(result.ToString());
+            }
+        }
+    }
+}
---
+++ PythonTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace PythonTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Startup...");
+            typeof(Python.Main).GetMethod("StartUp", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, null);
+            Console.WriteLine("Shutdown...");
+            typeof(Python.Main).GetMethod("ShutDown", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, null);
+        }
+    }
+}
