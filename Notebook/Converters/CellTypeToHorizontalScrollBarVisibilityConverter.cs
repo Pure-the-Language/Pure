fd527b80173c31afc6250f88a6bb522656798af1
Charles Zhang <szinubuntu@gmail.com>
2023-03-20 11:56:48 -0400

    [Notebook] Fix layout

---
+++ Notebook/Converters/CellTypeToHorizontalScrollBarVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Notebook.Converters
+{
+    class CellTypeToHorizontalScrollBarVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            switch ((CellType)value)
+            {
+                case CellType.CacheOutput:
+                    return ScrollBarVisibility.Auto;
+                case CellType.Markdown:
+                case CellType.CSharp:
+                case CellType.Python:
+                    return ScrollBarVisibility.Disabled;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ Notebook/Converters/CellTypeToTextWrappingConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Notebook.Converters
+{
+    class CellTypeToTextWrappingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            switch ((CellType)value)
+            {
+                case CellType.CacheOutput:
+                    return TextWrapping.NoWrap;
+                case CellType.Markdown:
+                case CellType.CSharp:
+                case CellType.Python:
+                    return TextWrapping.WrapWithOverflow;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
