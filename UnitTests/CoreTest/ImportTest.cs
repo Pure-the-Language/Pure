e052a3f6c823cec562dc33664faac9bfa1db8032
Chaojian Zhang <szinubuntu@gmail.com>
2023-09-01 11:34:47 -0400

    [Unit Tests] Implement ImportTest

--- UnitTests/CoreTest/ImportTest.cs
+++ UnitTests/CoreTest/ImportTest.cs
@@ -1,11 +1,18 @@
-﻿namespace CoreTest
+﻿using Core;
+
+namespace CoreTest
 {
     public class ImportTest
     {
         [Fact]
         public void ImportShouldBeAbleToDownloadNugetsAutomatically()
         {
-
+            new Interpreter(null, null, null, new[] {
+                """
+                Import(Newtonsoft.Json)
+                Help(Newtonsoft.Json)
+                """}, "LibraryImportTest").Start();
+            // If it passes through without problem, it's good
         }
     }
 }
