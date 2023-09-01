e2296b9eb37d76b7dc0fd66b6a421cbdd15492e9
Chaojian Zhang <szinubuntu@gmail.com>
2023-09-01 11:07:54 -0400

    [Unit Tests] Implement IncludeTest
    - Fix issue with `Include` implementation

--- UnitTests/CoreTest/IncludeTest.cs
+++ UnitTests/CoreTest/IncludeTest.cs
@@ -1,11 +1,27 @@
-﻿namespace CoreTest
+﻿using Core;
+
+namespace CoreTest
 {
     public class IncludeTest
     {
         [Fact]
         public void IncludeShouldWorkWithPathRelativeToScriptFile()
         {
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempDirectory);
+            string tempFile = Path.Combine(tempDirectory, "Script.cs");
+            string outputFile = Path.Combine(tempDirectory, "Output.txt");
 
+            string libraryFile = Path.Combine(tempDirectory, "Library.cs");
+            File.WriteAllText(libraryFile, $"""
+                File.WriteAllText(@"{outputFile}", "Hello World");
+                """);
+
+            new Interpreter(null, tempFile, null, new[] { 
+                """
+                Include(Library.cs)
+                """}, null).Start();
+            Assert.Equal("Hello World", File.ReadAllText(outputFile));
         }
     }
 }
