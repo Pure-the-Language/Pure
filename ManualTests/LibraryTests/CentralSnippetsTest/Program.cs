bc7ab1d20f2f46f431020d47c1fc438c19a34200
Chaojian Zhang <szinubuntu@gmail.com>
2023-10-27 15:49:06 -0400

    [Library Test] Setup interpreter for CentralSnippets test

--- ManualTests/LibraryTests/CentralSnippetsTest/Program.cs
+++ ManualTests/LibraryTests/CentralSnippetsTest/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace CentralSnippetsTest
+﻿using Core;
+
+namespace CentralSnippetsTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var interpreter = new Interpreter(null, null, null, null, null);
+            interpreter.Start();
+
             CentralSnippets.Main.Preview("Demos/HelloWorld.cs");
             CentralSnippets.Main.Download("Demos/HelloWorld.cs", "DownloadedHelloWorld.cs");
         }
